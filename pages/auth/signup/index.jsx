568378b616bf47d975d72f4e757813140a81d882
shafi0072 <safi29317@gmail.com>
2025-04-13 23:34:38 +0600

    added auth

--- pages/auth/signup/index.jsx
+++ pages/auth/signup/index.jsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material';
 import React from 'react';
-import SignUp from '../../../src/components/app/Authentication/Signup/index';
+import SignUp from '../../../src/components/app/Authentication/Signup/SignUp';
+
 
 const index = () => {
     return (
--- pages/auth/verify/index.jsx
+++ pages/auth/verify/index.jsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material';
 import React from 'react';
-import OTPVerify from '../../../src/components/app/Authentication/Verify/inedx';
+import OTPVerify from '../../../src/components/app/Authentication/Verify/OTPVerify';
+
 
 const index = () => {
     return (
